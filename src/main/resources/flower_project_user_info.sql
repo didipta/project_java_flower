e3ff17e4c13f46773e6dd56e9a6f0ead904495d7
didipta <66532706+didipta@users.noreply.github.com>
2022-08-10 22:34:05 +0600

    adsd

--- src/main/resources/flower_project_user_info.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO flower_project.user_info (first_name, last_name, email, phome_number, address, user_id, id) VALUES ('Dipta', 'Saha', 'dipta1110@gmail.com', '01912349456', 'korvanigong chittagong', 8, 1);
-INSERT INTO flower_project.user_info (first_name, last_name, email, phome_number, address, user_id, id) VALUES ('Shadik', 'Hasan', 'shadik123@gmail.com', '01799404026', '7i Sadek Road Basundhara R/A', 10, 3);
-INSERT INTO flower_project.user_info (first_name, last_name, email, phome_number, address, user_id, id) VALUES ('Chaity', 'Gosh', 'chaitygosh@gmail.com', '01957573434', 'dhaka', 11, 4);
